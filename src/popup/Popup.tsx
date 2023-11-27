ae56cff1c243895df34d1cc1d9b9e2807970daa8
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-11-27 16:54:27 +0530

    update

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,49 +1,42 @@
 import React, { useState } from 'react'
 
 function Popup() {
-    const [value, setValue] = useState<string>('');
+    const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleSave = () => {
-        chrome.storage.local.set({ key: value }, () => {
-            console.log('Value is set to ' + value);
-        });
-    }
+    const handleFileChange = (event) => {
+        // Get the selected file from the input
+        const file = event.target.files[0];
 
-    const handleLoad = () => {
-        chrome.storage.local.get(['key'], (result) => {
-            console.log('Value currently is ' + result.key);
-            setValue(result.key);
-        });
-    }
+        // Update the state with the selected file
+        setSelectedFile(file);
+    };
+
+    const handleUpload = () => {
+        // You can perform upload logic here, for example, send the file to a server
+
+        // For demonstration purposes, log the file details
+        if (selectedFile) {
+            console.log('Selected File:', selectedFile);
+        } else {
+            console.log('No file selected');
+        }
+    };
 
     return (
-        <div className='w-72'>
-            <div className='p-4'>
-                <h1 className='text-base mb-4 font-medium'>Chrome Extension with ReactJs</h1>
-
-                <div className='mb-10'>
-                    <input
-                        value={value}
-                        onChange={e => setValue(e.target.value)}
-                        className="bg-gray-50 border border-gray-200 w-full rounded-lg px-4 py-2 text-black focus:outline-none mb-5" />
-
-                    <div className='flex items-center justify-end'>
-                        <button onClick={handleSave} className="bg-green-500 hover:bg-green-400 transition duration-300 px-6 py-2 rounded-md text-white text-center">
-                            <span>Save</span>
-                        </button>
-                    </div>
-                </div>
-
-                <div className=''>
-                    <p className='text-sm mb-2'>Local Storage value</p>
-                    <div className='flex items-center space-x-2'>
-                        <button onClick={handleLoad} className="bg-orange-500 hover:bg-orange-400 transition duration-300 px-6 py-2 rounded-md text-white text-center">Load</button>
-                        <span className='text-lg font-semibold'>{value}</span>
-                    </div>
-                </div>
-            </div>
+        <div className="max-w-md mx-auto mt-8 p-4 bg-gray-100 rounded-md">
+            <input
+                type="file"
+                onChange={handleFileChange}
+                className="mb-4 p-2 border border-gray-300 rounded-md"
+            />
+            <button
+                onClick={handleUpload}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+                Upload
+            </button>
         </div>
-    )
+    );
 }
 
 export default Popup
